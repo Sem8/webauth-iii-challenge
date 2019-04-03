bee769057b4586f7c58a092f2cd8741e28e7bd93
Sem8 <mssemmi8@gmail.com>
2019-04-03 18:05:21 -0400

    login functionality with bcrypt authentication implemented

--- routers/auth/auth-router.js
+++ routers/auth/auth-router.js
@@ -20,4 +20,19 @@ router.post("/register", (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+    userdb('users').where({ username }).first().then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            // req.session.user = user;
+            res.status(200).json({ message: `Welcome ${user.username} you're logged in.`});
+        } else {
+            res.status(401).json({ message: 'Your credentials are invalid'});
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+})
+
 module.exports = router;
