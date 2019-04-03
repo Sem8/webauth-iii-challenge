135614904d708ebad1e803d14151e2239135d0da
Sem8 <mssemmi8@gmail.com>
2019-04-03 19:12:05 -0400

    Day 1 MVP done I think

--- routers/auth/restricted-middleware.js
+++ routers/auth/restricted-middleware.js
@@ -1,22 +1,42 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../../api/secrets.js");
 
-const userdb = require('../../database/dbConfig.js');
+const userdb = require("../../database/dbConfig.js");
 
 module.exports = (req, res, next) => {
-    const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-    if(username && password) {
-        userdb('users').where({ username }).first().then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next();
-            } else {
-                res.status(401).json({ message: 'Your credentials are invalid!'});
-            }
-        })
-        .catch(error => {
-            res.status(500).json({ message: `Ran into an unexpected error: ${error}`});
-        });
-    } else {
-        res.status(400).json({ message: `You didn't provide all the credentials`});
-    }
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ message: "Invalid token" });
+      } else {
+        req.decodedJwt = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "No token provided" });
+  }
 };
+
+// module.exports = (req, res, next) => {
+//     const { username, password } = req.headers;
+
+//     if(username && password) {
+//         userdb('users').where({ username }).first().then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)) {
+//                 next();
+//             } else {
+//                 res.status(401).json({ message: 'Your credentials are invalid!'});
+//             }
+//         })
+//         .catch(error => {
+//             res.status(500).json({ message: `Ran into an unexpected error: ${error}`});
+//         });
+//     } else {
+//         res.status(400).json({ message: `You didn't provide all the credentials`});
+//     }
+// };
